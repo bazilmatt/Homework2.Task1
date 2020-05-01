ce0e725b5754f6ed681f6860c60ca5fe0ed34a38
Vasiliy Matveev <bazilmat@yandex.ru>
2020-05-01 17:39:40 +0300

    Добавлены еще два исключения. Пока вызываются расскоментированием части кода. Дописать вызов.

--- src/main/java/HomeWork2Task1.java
+++ src/main/java/HomeWork2Task1.java
@@ -1,4 +1,3 @@
-import java.sql.SQLOutput;
 
 /**
  *
@@ -8,14 +7,25 @@ public class HomeWork2Task1 {
 
     public static void main(String[] args) {
         Object b = null;
+        int[] array = {0,1,2};
         try {
-            System.out.println("Hello world");
-            System.out.println("Hello world" + b.toString());
-        } catch (NullPointerException e){
+           // System.out.println(array[3]); - если расскоментировать выпадет исключение ArrayIndexOutOfBounds
+          //  f(); // Функция выкидывает собственное исключение MyException
+            System.out.println(b.getClass()); // строка порождает исключение NullPointException
+        } catch (NullPointerException e) {
             System.out.println("в переменной b значение null - вызван обработчик исключения");
+        } catch (ArrayIndexOutOfBoundsException e){
+            System.out.println("Выход за пределы массива");
+        } catch (MyException e){
+            System.out.println("поймано созданное исключениe");
         } finally {
             System.exit(1);
         }
 
     }
+
+    static class MyException extends RuntimeException {}
+
+    public static void f() throws MyException { throw new MyException();}
+
 }
